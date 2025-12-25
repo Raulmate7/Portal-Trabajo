3cc657635ac2985ad0bde299186e9887b15ad82d
Raulmate7 <raulmate7@gmail.com>
2025-12-25 14:22:06 +0100

    Configure Next.js to handle pg external package

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Le decimos a Next.js que 'pg' es una librería de servidor y no debe empaquetarla
+  serverExternalPackages: ['pg'],
 };
 
 export default nextConfig;
